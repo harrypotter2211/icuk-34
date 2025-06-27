cb43940a24d7c9da5cec5514dbfa76d42a9606a5
harrypotter2211 <yellaharish4@gmail.com>
2025-06-27 14:40:18 +0530

    Create SmokeTest.java

---
+++ src/test/java/com/valaxy/demo/SmokeTest.java
@@ -0,0 +1,11 @@
+package com.valaxy.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SmokeTest {
+    @Test
+    public void testSanity() {
+        assertTrue(true);
+    }
+}
